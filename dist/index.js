c3a49602318a0abd676e4612b6ddc6f9ea30d1fb
Yegor Kudinov <bp.headwire@gmail.com>
2019-05-21 18:46:20 +0300

    Enky invoices layout

--- dist/index.js
+++ dist/index.js
@@ -1,137 +1,136 @@
-const pdfKit = require('pdfkit');
-const moment = require('moment');
-const numeral = require('numeral');
-const i18n = require('./i18n');
+'use strict';
 
-const TEXT_SIZE = 8;
-const CONTENT_LEFT_PADDING = 50;
+var pdfKit = require('pdfkit');
+var moment = require('moment');
+var numeral = require('numeral');
+var i18n = require('./i18n');
 
-function PDFInvoice({
-  company, // {phone, email, address}
-  customer, // {name, email, phone, address}
-  items, // [{amount, name, description}]
-  invoiceDate,
-}){
-  const date = new Date(invoiceDate);
-  const charge = {
-    createdAt: `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`,
-    amount: items.reduce((acc, item) => acc + item.amount, 0),
+var TEXT_SIZE = 10;
+var CONTENT_LEFT_PADDING = 50;
+
+function PDFInvoice(_ref) {
+  var company = _ref.company,
+      customer = _ref.customer,
+      items = _ref.items,
+      invoiceDate = _ref.invoiceDate,
+      invoiceNo = _ref.invoiceNo,
+      orderId = _ref.orderId;
+
+  var date = new Date(invoiceDate);
+  var charge = {
+    createdAt: date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear(),
+    amount: items.reduce(function (acc, item) {
+      return acc + item.amount;
+    }, 0)
   };
-  const doc = new pdfKit({size: 'A4', margin: 50});
+  var doc = new pdfKit({ size: 'A4', margin: 50 });
 
   doc.fillColor('#333333');
 
-  const translate = i18n[PDFInvoice.lang];
+  var translate = i18n[PDFInvoice.lang];
   moment.locale(PDFInvoice.lang);
 
-  const divMaxWidth = 550;
-  const table = {
+  var divMaxWidth = 550;
+  var table = {
     x: CONTENT_LEFT_PADDING,
-    y: 200,
-    inc: 150,
+    y: 350,
+    inc: 70
   };
 
   return {
-    genHeader(){
-      doc
-       .fontSize(20)
-       .text(company.name, CONTENT_LEFT_PADDING, 50);
+    genHeader: function genHeader() {
+      doc.image('server/public/img/HS-Security.logo.png');
 
-      const borderOffset = doc.currentLineHeight() + 70;
-
-      doc
-        .fontSize(16)
-        .fillColor('#cccccc')
-        .text(moment(date).format('MMMM, DD, YYYY'), CONTENT_LEFT_PADDING, 50, {
-          align: 'right',
-        })
-        .fillColor('#333333');
-
-      doc
-        .strokeColor('#cccccc')
-        .moveTo(CONTENT_LEFT_PADDING, borderOffset)
-        .lineTo(divMaxWidth, borderOffset);
+      doc.fontSize(11).font('Helvetica-Bold').text('FROM:', CONTENT_LEFT_PADDING, 180);
+      doc.font('Helvetica').text(company.name || '');
+      doc.text(company.address || '');
+      doc.text(company.city || '');
+      doc.text(company.country || '');
+      doc.text('VAT: ' + (company.vat || ""));
     },
-
-    genFooter(){
-      doc.fillColor('#cccccc');
-
-      doc
-        .fontSize(12)
-        .text(company.name, CONTENT_LEFT_PADDING, 500);
-
-      doc.text(company.address);
-      doc.text(company.phone);
-      doc.text(company.email);
-
-      doc.fillColor('#333333');
+    genCustomerInfos: function genCustomerInfos() {
+      doc.font('Helvetica-Bold').text('TO:', CONTENT_LEFT_PADDING * 5, 180);
+      doc.font('Helvetica').text(customer.name || '');
+      doc.text(customer.address || '');
+      doc.text(customer.city || '');
+      doc.text(customer.country || '');
+      doc.text('VAT: ' + (customer.vat || ""));
     },
-
-    genCustomerInfos(){
-      doc
-        .fontSize(TEXT_SIZE)
-        .text(translate.chargeFor, CONTENT_LEFT_PADDING, 400);
-
-      doc.text(`${customer.name}`);
-      doc.text(`${customer.address}`);
-      doc.text(`${customer.phone}`);
-      doc.text(`${customer.email}`);
+    genDate: function genDate() {
+      doc.fillColor('#000').text('Invoice Date: ' + moment(date).format('DD.MM.YYYY'), CONTENT_LEFT_PADDING, 290, {
+        align: 'right'
+      }).fillColor('#000');
     },
+    genInvoiceNo: function genInvoiceNo() {
+      doc.font('Helvetica-Bold').text('INVOICE No: ' + invoiceNo, CONTENT_LEFT_PADDING, 290);
 
-    genTableHeaders(){
-      [
-        'amount',
-        'name',
-        'description',
-        'quantity',
-      ].forEach((text, i) => {
-        doc
-          .fontSize(TEXT_SIZE)
-          .text(translate[text], table.x + i * table.inc, table.y);
+      doc.font('Helvetica').text('Order ID: ' + orderId);
+    },
+    genFooter: function genFooter() {
+      var borderOffset = doc.currentLineHeight() + 70;
+
+      var note = 'Thank you for your order. Our General Terms and Conditions apply which are published at http://www.hs-securityware.com; Our conditions are considered to have been accepted at the latest when the goods are accepted. Exclusion of property: The good remains until payment of all receivables towards the purchaser of the business relation, including auxiliary receivables, claims for damages and payment of checks and bills of exchange, the property of the seller.';
+
+      doc.font('Helvetica').fontSize(8).text(note, CONTENT_LEFT_PADDING, doc.y + 20).moveDown(2);
+
+      doc.fontSize(12).text('Delivery: Online EnkyOn.de portal');
+      doc.text('Payment: due immediately upon receipt');
+      doc.text('You can pay the bill by credit card or transfer.');
+      doc.text('Bank: Commerzbank AG');
+      doc.text('Bank address: Südstr. 14-16, 31515 Wunstorf');
+      doc.text('IBAN: DE88 2504 0066 0272 3658 00');
+      doc.text('BIC: COBADEFFXXX');
+      doc.text('Account name: HS-Security Ware GmbH');
+      doc.text('Account no.: 272 365 800');
+    },
+    genTableHeaders: function genTableHeaders() {
+      ['quantity', 'productNo', 'productName', 'createdAt', 'amount'].forEach(function (text, i) {
+        var n = text === 'createdAt' || text === 'amount' ? 160 : 0;
+
+        doc.fontSize(TEXT_SIZE).font('Helvetica').text(translate[text], table.x + n + i * table.inc, table.y);
       });
     },
-
-    genTableRow(){
-      items
-        .map(item => Object.assign({}, item, {
-          amount: '€' + numeral(item.amount)
-        }))
-        .forEach((item, itemIndex) => {
-          [
-            'amount',
-            'name',
-            'description',
-            'quantity',
-          ].forEach((field, i) => {
-            doc
-              .fontSize(TEXT_SIZE)
-              .text(item[field], table.x + i * table.inc, table.y + TEXT_SIZE + 6 + itemIndex * 20);
-          });
+    genTableRow: function genTableRow() {
+      items.map(function (item) {
+        return Object.assign({}, item, {
+          amount: (item.currency === 'USD' ? '$ ' : '€ ') + numeral(item.amount)
         });
-    },
+      }).forEach(function (item, itemIndex) {
+        ['quantity', 'productNo', 'productName', 'createdAt', 'amount'].forEach(function (field, i) {
+          if (field === 'createdAt') {
+            item[field] = moment(item[field]).format('DD.MM.YYYY');
+          }
+          var n = field === 'createdAt' || field === 'amount' ? 160 : 0;
 
-    genTableLines(){
-      const offset = doc.currentLineHeight() + 2;
-      doc
-        .moveTo(table.x, table.y + offset)
-        .lineTo(divMaxWidth, table.y + offset)
-        .stroke();
+          doc.fontSize(TEXT_SIZE).text(item[field], table.x + n + i * table.inc, table.y + TEXT_SIZE + 6 + itemIndex * 15);
+        });
+      });
     },
-
-    generate(){
+    genTotal: function genTotal() {
+      doc.font('Helvetica').text('TOTAL: ' + charge.amount, CONTENT_LEFT_PADDING, doc.y + 20);
+    },
+    genTableLines: function genTableLines() {
+      var offset = doc.currentLineHeight() + 2;
+      doc.moveTo(table.x, table.y + offset).lineTo(divMaxWidth, table.y + offset).stroke();
+    },
+    generate: function generate() {
       this.genHeader();
+      this.genCustomerInfos();
+      this.genDate();
+      this.genInvoiceNo();
       this.genTableHeaders();
       this.genTableLines();
       this.genTableRow();
-      this.genCustomerInfos();
+      this.genTotal();
       this.genFooter();
 
       doc.end();
     },
 
-    get pdfkitDoc(){
+
+    get pdfkitDoc() {
       return doc;
-    },
+    }
   };
 }
 
